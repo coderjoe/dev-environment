4f880a061ba35d824868681818a8f126b06238f0
Joe Bauser <coderjoe@coderjoe.net>
2019-10-05 19:19:34 -0400

    Fix a bug with git user variables
    This commit updates previous commits to fix a few bugs related to the
    git user variables.
    
    * Always set the variables even when a tag is being used
    * Make sure we're setting git_name not the incorrect git_username
    * Update the gitconfig to generate a valid config even if nothing is
      provided

--- scripts/set_user_variables.sh
+++ scripts/set_user_variables.sh
@@ -6,14 +6,14 @@ if [ -s "$VAR_FILENAME" ]; then
 	exit 0
 fi
 
-read -p 'Git Name: ' git_username
+read -p 'Git Name: ' git_name
 read -p 'Git Email: ' git_email
 read -p 'Git GPG Signing Key: ' git_signingkey
 read -p 'Github Username: ' github_username
 read -p 'Github API Token: ' github_token
 
 echo '---' > "$VAR_FILENAME"
-[[ ! -z "$git_username" ]] && echo "git_name: $git_username" >> "$VAR_FILENAME"
+[[ ! -z "$git_name" ]] && echo "git_name: $git_name" >> "$VAR_FILENAME"
 [[ ! -z "$git_email" ]] && echo "git_email: $git_email" >> "$VAR_FILENAME"
 [[ ! -z "$git_signingkey" ]] && echo "git_signingkey: $git_signingkey" >> "$VAR_FILENAME"
 [[ ! -z "$github_username" ]] && echo "github_username: $github_username" >> "$VAR_FILENAME"
