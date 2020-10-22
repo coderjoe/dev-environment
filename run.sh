0b7e6dbb1b630545b89791068f2a68ec41b244b7
Joe Bauser <coderjoe@coderjoe.net>
2020-10-22 19:44:44 -0400

    Add SKIP_ANSIBLE_INSTALL environment var
    As of Ubuntu 20.04 in late 2020 there is a bug due to a patched version
    of python virtualenv which is preventing the ansible project from
    supporting Ubuntu 20.04 as a valid PPA. As a result the bootstrap
    process fails. This means I need to manually disable the check until the
    PPA is fixed.
    
    This commit allows me to do so by setting the environment variable
    SKIP_ANSIBLE_INSTALL to any value prior to runnign run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-./scripts/install_ansible.sh
-if [ $? -ne 0 ]; then
-	echo "An error occurred while bootstrapping or upgrading ansible!"
-	echo "Please resolve the errors and try again."
-	exit 1;
+if [[ $SKIP_ANSIBLE_INSTALL && ${SKIP_ANSIBLE_INSTALL-x} ]]; then
+	echo "SKIP_ANSIBLE_INSTALL is defined!"
+	echo "Skipping the ansible boostrap process,"
+else
+	./scripts/install_ansible.sh
+	if [ $? -ne 0 ]; then
+		echo "An error occurred while bootstrapping or upgrading ansible!"
+		echo "Please resolve the errors and try again."
+		exit 1;
+	fi
 fi
 
 echo "Loading user variables..."
--- scripts/run_playbook.sh
+++ scripts/run_playbook.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ansible-playbook --ask-become-pass playbooks/local.yml $@
+if [[ $SKIP_ANSIBLE_INSTALL && ${SKIP_ANSIBLE_INSTALL-x} ]]; then
+	ansible-playbook --extra-vars "skip_ansible_install=true" --ask-become-pass playbooks/local.yml $@
+else
+	ansible-playbook --ask-become-pass playbooks/local.yml $@
+fi
