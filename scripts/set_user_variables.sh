0bd6959bb66dd309822e0c3801909b4afec9e2cb
Joe Bauser <coderjoe@coderjoe.net>
2022-07-03 14:38:31 -0400

    Update user vars to prompt for everything used

--- scripts/set_user_variables.sh
+++ scripts/set_user_variables.sh
@@ -9,6 +9,8 @@ fi
 read -p 'Git Name: ' git_name
 read -p 'Git Email: ' git_email
 read -p 'Git GPG Signing Key: ' git_signingkey
+read -p 'Bazaar Email: ' bzr_email
+read -p 'Launchpad Username: ' launchpad_username
 read -p 'Github Username: ' github_username
 read -p 'Github API Token: ' github_token
 
@@ -16,6 +18,8 @@ echo '---' > "$VAR_FILENAME"
 [[ ! -z "$git_name" ]] && echo "git_name: $git_name" >> "$VAR_FILENAME"
 [[ ! -z "$git_email" ]] && echo "git_email: $git_email" >> "$VAR_FILENAME"
 [[ ! -z "$git_signingkey" ]] && echo "git_signingkey: $git_signingkey" >> "$VAR_FILENAME"
+[[ ! -z "$bzr_email" ]] && echo "bzr_email: $bzr_email" >> "$VAR_FILENAME"
+[[ ! -z "$launchpad_username" ]] && echo "launchpad_username: $launchpad_username" >> "$VAR_FILENAME"
 [[ ! -z "$github_username" ]] && echo "github_username: $github_username" >> "$VAR_FILENAME"
 [[ ! -z "$github_token" ]] && echo "github_token: $github_token" >> "$VAR_FILENAME"
 
